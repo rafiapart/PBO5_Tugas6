d8a0d62cddf5f8d1b7a369064118104287b99b15
rafii <decik733@gmail.com>
2025-11-21 19:09:13 +0700

    no 1

---
+++ no1.java
@@ -0,0 +1,9 @@
+package tugas6pbo;
+
+public class no1 {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        int[] b = {1, 2, 3, 4, 5};
+        int c[] = new int[]{10, 20, 30};
+    }
+}
